131dcccfed1d5e9daca1c566617113c3204a9355
CHATO <colochocristian@outlook.com>
2025-03-10 11:02:16 -0600

    Agregar archivos de proyecto.

---
+++ WorksSP.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorksSP.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
